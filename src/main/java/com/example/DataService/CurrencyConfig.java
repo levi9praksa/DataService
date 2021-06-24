7567f330f4daaeed182ce0e2e85a075389239985
Naffets <nafetssurducan@gmail.com>
2021-06-24 12:06:45 +0200

    next commit

--- src/main/java/com/example/DataService/CurrencyConfig.java
+++ src/main/java/com/example/DataService/CurrencyConfig.java
@@ -7,8 +7,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.repository.CurrencyRepository;
 
-import model.Currency;
-
 @Configuration
 public class CurrencyConfig {
 	
@@ -16,17 +14,7 @@ public class CurrencyConfig {
 	@RequestMapping("/putCurrencies")
 	CommandLineRunner commandLineRunner(CurrencyRepository repository) {
 		return args -> {
-			/*Currency c = new Currency();
-
-			c.setId(2);
-			c.setName("ethereum");
-			c.setSymbol("ETH");
-			c.setPriceusd(300);
-			c.setChangepercent24h(2);
-			c.setThreshold(15);
-			repository.save(c);
 			
-			System.out.print("Done!");*/
 		};
 	}
 }
---
+++ src/main/java/com/example/controllers/CurrencyController.java
@@ -0,0 +1,92 @@
+package com.example.controllers;
+
+import java.io.IOException;
+
+import javax.annotation.PostConstruct;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.repository.CurrencyRepository;
+
+import model.Currency;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+@RestController
+public class CurrencyController {
+
+	@Autowired
+	CurrencyRepository cr;
+
+	@RequestMapping("/putCurrencies")
+	@PostConstruct
+	public void putCurrencies() {
+
+		OkHttpClient client = new OkHttpClient().newBuilder().build();
+		Request request = new Request.Builder().url("https://api.coincap.io/v2/assets/").method("GET", null).build();
+		Response response;
+		try {
+			response = client.newCall(request).execute();
+			String line = response.body().string();
+			String[] tokens = line.split("[},:{]");
+
+			int i = 0;
+			int j = 0;
+			int id = 0;
+
+			Currency c = new Currency();
+
+			for (String t : tokens) {
+				String[] s = t.split("[\"]");
+				if (i == 30) {
+					i = 3;
+					id++;
+					c.setId(id);
+					c.setThreshold(0);
+					cr.save(c);
+
+					c = new Currency();
+				}
+
+				switch (i) {
+
+				case 4:
+					c.setName(s[1]);
+					break;
+
+				case 8:
+					c.setSymbol(s[1]);
+					break;
+
+				case 20:
+					c.setPriceusd(Float.parseFloat(s[1]));
+					break;
+
+				case 22:
+					c.setChangepercent24h(Float.parseFloat(s[1]));
+					break;
+
+				default:
+					break;
+
+				}
+
+				if (j == 300)
+					break;
+				i++;
+				j++;
+			}
+
+			System.out.print("Storing data into database completed.");
+
+		} catch (
+
+		IOException e) {
+			e.printStackTrace();
+		}
+
+	}
+}
